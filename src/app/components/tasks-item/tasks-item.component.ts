07c311f2d21b4b0a94562fe2d9b25a22042d20b3
vieradaniel <dariennx@hotmail.com>
2021-11-22 20:31:21 -0300

    item list

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {Task} from "../../Task"
+import {TASKS} from "../../mock-tasks"
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+
+  @Input() task:Task= TASKS[0]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
